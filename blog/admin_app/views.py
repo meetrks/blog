85f7010514194eb14ac3785aafa8d502b5fe8fa3
Rahul <skrsinghrahul@gmail.com>
2018-08-14 00:03:34 +0530

    Add: search, filter to admin view

--- blog/admin_app/views.py
+++ blog/admin_app/views.py
@@ -1,5 +1,6 @@
 import time
 
+from django.db.models import Q
 from django.utils.text import slugify
 from rest_framework import status, serializers
 from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
@@ -15,9 +16,27 @@ class BlogListView(ListAPIView, CreateAPIView):
     permission_classes = (IsSuperUser,)
     serializer_class = BlogSerializer
 
+    def get_filters(self):
+        filters = {}
+        query_params = self.request.query_params
+        reported_date_from = query_params.get('reported_date_from')
+        reported_date_to = query_params.get('reported_date_to')
+        if reported_date_from and reported_date_to:
+            reported_date_to = int(reported_date_to) + 86400
+            filters.update({"published_time__range": [reported_date_from, reported_date_to]})
+        topic = query_params.get('topic')
+        if topic:
+            filters.update({"related_topics": topic})
+        self.q = query_params.get('q')
+        self.page_size = query_params.get('page_size', 10)
+        return filters
+
     def get(self, request):
-        queryset = Blog.objects.all()
-        paginator, result = pagination(queryset, request)
+        queryset = Blog.objects.filter(**self.get_filters())
+        if self.q:
+            queryset = queryset.filter(Q(title__icontains=self.q) | Q(description__icontains=self.q) |
+                                       Q(related_topics__topic__icontains=self.q)).distinct()
+        paginator, result = pagination(queryset, request, page_size=self.page_size)
         serializer = self.get_serializer(result, many=True)
         response_data = serializer.data
         return paginator.get_paginated_response(response_data)
--- blog/log/urls.py
+++ blog/log/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from log.views import *
 
 urlpatterns = [
-    url('', BlogView.as_view(), name='blog_list_view'),
+    url('^$', BlogView.as_view(), name='blog_list_view'),
     url('^blog/(?P<id>[a-zA-Z0-9-]{36})/(?P<title_slug>.+)/$', BlogView.as_view(), name='blog_detail_view'),
     url('^comment/(?P<blog_id>[a-zA-Z0-9-]{36})/$', CommentView.as_view(), name='comment_view'),
 ]
